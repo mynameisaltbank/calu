672c4b0936954b5c9dccf52828f43fa41eb73053
mynameisaltbank <apa7177153@gmail.com>
2026-07-10 13:40:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ from flask import Flask, render_template, request, jsonify, send_file
 import google.generativeai as genai
 import pandas as pd
 
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
+
 
 
 
